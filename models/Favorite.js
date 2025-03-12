6435db44a03dc5c1fa78ee493a13f64598da37cb
Elvam03 <velmakemunto03@gmail.com>
2025-03-12 11:11:07 +0300

    fixed error

--- models/Favorite.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const FavoriteSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    favorites: [{ type: String }] // Array of favorite resource IDs
-});
-
-module.exports = mongoose.model('Favorite', FavoriteSchema);
